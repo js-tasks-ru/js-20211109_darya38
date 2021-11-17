9dfdeeb8962cf3ebd933e069b8ddbc3943f6f4a3
Daria Volkova <daria911@yandex.ru>
2021-11-17 20:11:04 +0300

    Solve 03_01 task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const properties = path.split('.');
 
+  return (object) => {
+    let currentObject = object;
+   
+    for (const prop of properties) {
+      if (currentObject === undefined) {
+        break;
+      }
+
+      currentObject = currentObject[prop];
+    }
+
+    return currentObject;
+  };
 }
