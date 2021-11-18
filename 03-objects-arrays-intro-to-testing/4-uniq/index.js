feee5071648eebf9b4b8517498b071cb6ca5d30e
Daria Volkova <daria911@yandex.ru>
2021-11-18 20:05:09 +0300

    Solve 03_04 task

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,11 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (arr == undefined) {
+    return [];
+  }
 
+  const result = Array.from(new Set(arr));
+
+  return result;
 }
